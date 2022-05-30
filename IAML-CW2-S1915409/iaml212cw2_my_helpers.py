ef75711ba362d6edb8b92a94161814c68dacf9cf
Zihao Cai <40333535+IrwinChay@users.noreply.github.com>
2022-05-30 21:33:33 +0100

    Implementation

---
+++ IAML-CW2-S1915409/iaml212cw2_my_helpers.py
@@ -0,0 +1,8 @@
+
+##########################################################
+#  Python module template for helper functions of your own (IAML Level 10)
+#  Note that:
+#  - Those helper functions of your own for Questions 1 and 2 should be defined in this file.
+#  - You can decide function names by yourself.
+#  - You do not need to include this header in your submission.
+##########################################################
